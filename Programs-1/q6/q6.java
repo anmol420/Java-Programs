a9cb3fd01a71914bd66905f49766fab07be0c376
Anmol Anand <anmolanand2k15@gmail.com>
2022-02-05 13:40:53 +0530

    Final Commit

--- Programs-1/q6/q6.java
+++ Programs-1/q6/q6.java
@@ -1,2 +1,39 @@
 import java.util.*;
-class q6 {}
+
+class q6 {
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter A String");
+            String s = sc.next();
+
+            String str = s.toLowerCase();
+            int len = str.length();
+
+            String sortedStr = ""; // Empty String
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                for (int i = 0; i < len; i++) {
+                    char strCh = str.charAt(i);
+                    if (ch == strCh) {
+                        sortedStr += strCh;
+                    }
+                }
+            }
+
+            System.out.println("Alphabetical order:");
+            System.out.println(sortedStr);
+
+            String filledStr = ""; // Empty String
+            for (int i = 0; i < len - 1; i++) {
+                char strCh = sortedStr.charAt(i);
+                filledStr += strCh;
+                for (int j = strCh + 1; j < sortedStr.charAt(i + 1); j++) {
+                    filledStr += (char) j;
+                }
+            }
+            filledStr += sortedStr.charAt(len - 1);
+
+            System.out.println("Filled String:");
+            System.out.println(filledStr);
+        }
+    }
+}
